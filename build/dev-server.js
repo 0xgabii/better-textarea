192893f7221f872fcdc15db773979982c67d4a73
Go Eun-Seok <gomonk3037@gmail.com>
2018-02-01 14:57:54 +0900

    Force page reload when html template changes

--- build/dev-server.js
+++ build/dev-server.js
@@ -18,7 +18,14 @@ const hotMiddleware = require('webpack-hot-middleware')(compiler, {
 app.use(devMiddleware);
 
 // enable hot-reload and state-preserving
-// compilation error display
 app.use(hotMiddleware);
+
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
